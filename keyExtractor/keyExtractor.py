151fb8bbb3427de24c460e7b4ba5909cb44ca6a6
ab-chaudhry <69024550+ab-chaudhry@users.noreply.github.com>
2020-07-31 03:55:38 +0100

    Update keyExtractor.py

--- keyExtractor/keyExtractor.py
+++ keyExtractor/keyExtractor.py
@@ -1,8 +1,8 @@
 import pyzipper
 
 def main():
-	inFile = 'C:\\Users\\markhor\\Desktop\\ProductSubmission\\attempt3.zip'
-	outDir = 'C:\\Users\\markhor\\Desktop\\ProductSubmission\\\keyExtractor\\extracts'
+	inFile = 'C:\\Users\\user1\\Desktop\\ProductSubmission\\attempt3.zip'
+	outDir = 'C:\\Users\\user1\\Desktop\\ProductSubmission\\\keyExtractor\\extracts'
 	userInput = input("Enter password for the ZIP file: ")
 	pwd = bytes(userInput, 'utf-8')
 	print("pwd: ", pwd)
