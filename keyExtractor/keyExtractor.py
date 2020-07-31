a23ba422cfa4dd618aa38d4f42942eb7f1bbc29f
ab-chaudhry <69024550+ab-chaudhry@users.noreply.github.com>
2020-07-31 03:54:42 +0100

    Add files via upload

---
+++ keyExtractor/keyExtractor.py
@@ -0,0 +1,15 @@
+import pyzipper
+
+def main():
+	inFile = 'C:\\Users\\markhor\\Desktop\\ProductSubmission\\attempt3.zip'
+	outDir = 'C:\\Users\\markhor\\Desktop\\ProductSubmission\\\keyExtractor\\extracts'
+	userInput = input("Enter password for the ZIP file: ")
+	pwd = bytes(userInput, 'utf-8')
+	print("pwd: ", pwd)
+	open('keys.txt', 'w').close() #Clears the contents of the text file containing keys
+	with pyzipper.AESZipFile(inFile) as fileIn:
+		fileIn.setpassword(pwd)
+		list_files = fileIn.namelist()
+		fileIn.extractall(outDir)
+		
+if __name__ == '__main__': main()
